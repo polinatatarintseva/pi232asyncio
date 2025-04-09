2892b2d220cf9c43ac12a296f78cd370e2ce4a8f
polinatatarintseva <pollytat2005@gmail.com>
2025-04-09 13:22:15 +0300

    correct stop

--- server.py
+++ server.py
@@ -110,9 +110,6 @@ async def stop_server_when_no_clients(server):
  
 
  
-
- 
-
 async def read_server_commands(loop):
  
     """
@@ -127,9 +124,6 @@ async def read_server_commands(loop):
  
         # Читаем команду с консоли в отдельном потоке, чтобы не блокировать цикл событий
  
-
- 
-
         cmd = await loop.run_in_executor(None, input)
  
         if cmd.strip() == 'stop':
@@ -137,6 +131,7 @@ async def read_server_commands(loop):
             print("Команда 'stop' получена. Остановка сервера после отключения всех клиентов.")
  
             stop_server = True  # Устанавливаем флаг остановки сервера
+ 
             break
  
 
@@ -159,43 +154,66 @@ async def main():
  
 
  
-
     loop = asyncio.get_running_loop()
+ 
+
+ 
+
+    # Создаем задачи
+ 
+
+    server_task = loop.create_task(server.serve_forever())
+ 
+
+    command_task = loop.create_task(read_server_commands(loop))
+ 
+
+    stop_task = loop.create_task(stop_server_when_no_clients(server))
+ 
+
+
+ 
+
+    tasks = [server_task, command_task, stop_task]
  
 
 
  
     try:
- 
-        # Запускаем сервер и функции обслуживания команд и остановки сервера параллельно
- 
-        await asyncio.gather(
- 
-            server.serve_forever(),        # Сервер принимает подключения
+        # Ждем завершения задач
  
 
+        await asyncio.gather(*tasks)
  
 
-            read_server_commands(loop),    # Читаем команды с консоли
+    except asyncio.CancelledError:
  
-            stop_server_when_no_clients(server),  # Проверяем условие остановки сервера
+
+        # Здесь мы ловим CancelledError, который возникает при отмене задач
  
-        )
+
+        pass
     except KeyboardInterrupt:
  
         # Обрабатываем прерывание по Ctrl+C
  
         print("Сервер прерван пользователем (Ctrl+C)")
- 
-        server.close()  # Останавливаем сервер
+        server.close()
  
         await server.wait_closed()
+ 
+
+        for task in tasks:
+ 
+
+            task.cancel()
+ 
+
+        await asyncio.gather(*tasks, return_exceptions=True)
  
     finally:
  
         print("Сервер остановлен")
- 
-
  
 if __name__ == '__main__':
  
